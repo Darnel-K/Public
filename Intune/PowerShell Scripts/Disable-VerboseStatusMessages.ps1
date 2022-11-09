96de31165b68317e1945848d486cd21da9a72b7a
Darnel Kumar <darnel.kumar@dgk.one>
2022-11-09 10:17:01 +0000

    Create Disable-VerboseStatusMessages.ps1

---
+++ Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
@@ -0,0 +1,50 @@
+<#
+ * ############################################################################
+ * Filename: \Intune\PowerShell Scripts\Disable-VerboseStatusMessages.ps1
+ * Repository: Public
+ * Created Date: Wednesday, November 9th 2022, 9:20:26 AM
+ * Original Author: Darnel Kumar
+ * Author Github: https://github.com/Darnel-K
+ *
+ * Copyright (c) 2022 Darnel Kumar
+ * ############################################################################
+#>
+$RegKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$VerboseProperty = "verbosestatus"
+$StatusMsgProperty = "DisableStatusMessages"
+
+if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $VerboseProperty) {
+    try {
+        Set-Itemproperty -path $RegKey -Name $VerboseProperty -value '0'
+    }
+    catch {
+        $_.Exception.Message
+    }
+}
+else {
+    try {
+        New-ItemProperty -path $RegKey -Name $VerboseProperty -value '0' -Type DWORD
+    }
+    catch {
+        $_.Exception.Message
+    }
+
+}
+if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $StatusMsgProperty) {
+    try {
+        Set-Itemproperty -path $RegKey -Name $StatusMsgProperty -value '1'
+    }
+    catch {
+        $_.Exception.Message
+    }
+}
+else {
+    try {
+        New-ItemProperty -path $RegKey -Name $StatusMsgProperty -value '1' -Type DWORD
+    }
+    catch {
+        $_.Exception.Message
+    }
+
+}
+Exit $LASTEXITCODE
