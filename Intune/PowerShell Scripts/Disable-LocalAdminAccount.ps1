408491eb74a90c42019558047e8a1a449843a55e
Darnel Kumar <darnel.kumar@dgk.one>
2022-11-09 10:16:58 +0000

    Create Disable-LocalAdminAccount.ps1

---
+++ Intune/PowerShell Scripts/Disable-LocalAdminAccount.ps1
@@ -0,0 +1,26 @@
+<#
+ * ############################################################################
+ * Filename: \Intune\PowerShell Scripts\Disable-LocalAdminAccount.ps1
+ * Repository: Public
+ * Created Date: Wednesday, November 9th 2022, 9:34:32 AM
+ * Original Author: Darnel Kumar
+ * Author Github: https://github.com/Darnel-K
+ *
+ * Copyright (c) 2022 Darnel Kumar
+ * ############################################################################
+#>
+$User = "Administrator"
+try {
+    $Result = (Get-LocalUser -Name $user -ErrorAction Stop).Enabled
+    try {
+        if ($Result) {
+            Disable-LocalUser -Name $User
+        }
+    }
+    catch {
+        $_.Exception.Message #in case disable fails
+    }
+}
+catch {
+    $_.Exception.Message #if user doesnt exist
+}
