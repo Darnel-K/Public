391e5ddf43444d54e35415c57d0889b564a09c99
Darnel Kumar <darnel.kumar@dgk.one>
2022-12-09 17:05:09 +0000

    Update Get-OrgHierarchy.ps1

--- PowerShell Scripts/Get-OrgHierarchy.ps1
+++ PowerShell Scripts/Get-OrgHierarchy.ps1
@@ -1,66 +1,18 @@
 <#
  * ############################################################################
- * Filename: \PowerShell Scripts\Get-AzureAdHierarchy.ps1
+ * Filename: \PowerShell Scripts\Get-OrgHierarchy.ps1
  * Repository: Public
  * Created Date: Monday, December 5th 2022, 4:20:07 PM
- * Last Modified: Monday, December 5th 2022, 4:23:40 PM
+ * Last Modified: Friday, December 9th 2022, 4:23:13 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2022 Darnel Kumar
  * ############################################################################
 #>
-function Get-ModuleInstallStatus {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Name
-    )
-    $Module = Get-Module -Name $Name -ListAvailable -ErrorAction SilentlyContinue
-    if ($Module.count -eq 0) {
-        return $false
-    }
-    else {
-        return $true
-    }
-}
-
-
-if (Get-ModuleInstallStatus -Name PSWriteHTML) {
-    try {
-
-    }
-    catch {
-
-    }
-}
-else {
-    Install-Module PSWriteHTML -Force
-    try {
-
-    }
-    catch {
-
-    }
-}
-
-if (Get-ModuleInstallStatus -Name AzureAD) {
-    try {
-        $SessionInfo = Connect-AzureAD -ErrorAction Stop | Select-Object Account, TenantId, TenantDomain
-    }
-    catch {
-        Write-Warning "Authentication Failed..."
-        Write-Warning $Error[0]
-        Exit 1
-    }
-}
-else {
-    Install-Module AzureAD -Force
-    try {
-        $SessionInfo = Connect-AzureAD -ErrorAction Stop | Select-Object Account, TenantId, TenantDomain
-    }
-    catch {
-        Write-Warning "Authentication Failed..."
-        Write-Warning $Error[0]
-        Exit 1
-    }
-}
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [TypeName]
+    $ParameterName
+)
