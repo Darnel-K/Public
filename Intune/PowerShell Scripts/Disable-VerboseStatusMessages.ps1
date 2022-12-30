b3f99e84f254eecdbee9633d404fd22a2a466ad3
Darnel Kumar <darnel.kumar@dgk.one>
2022-12-30 17:08:01 +0000

    Update Disable-VerboseStatusMessages.ps1

--- Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
+++ Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
@@ -3,49 +3,48 @@
  * Filename: \Intune\PowerShell Scripts\Disable-VerboseStatusMessages.ps1
  * Repository: Public
  * Created Date: Wednesday, November 9th 2022, 9:20:26 AM
- * Last Modified: Wednesday, November 9th 2022, 2:13:05 PM
+ * Last Modified: Friday, December 30th 2022, 5:06:33 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2022 Darnel Kumar
  * ############################################################################
 #>
-$RegKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-$VerboseProperty = "verbosestatus"
-$StatusMsgProperty = "DisableStatusMessages"
+$RegData = @(
+    [PSCustomObject]@{
+        Path  = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+        Name  = "verbosestatus"
+        Value = "0"
+        Type  = "DWord"
+    }
+    [PSCustomObject]@{
+        Path  = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+        Name  = "DisableStatusMessages"
+        Value = "1"
+        Type  = "DWord"
+    }
+)
 
-if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $VerboseProperty) {
-    try {
-        Set-Itemproperty -path $RegKey -Name $VerboseProperty -value '0'
+foreach ($i in $RegData) {
+    if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
+        try {
+            Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
+        }
+        catch {
+            Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
+            Write-Verbose $Error[0]
+            Exit 1
+        }
     }
-    catch {
-        $_.Exception.Message
-    }
-}
-else {
-    try {
-        New-ItemProperty -path $RegKey -Name $VerboseProperty -value '0' -Type DWORD
-    }
-    catch {
-        $_.Exception.Message
-    }
-
-}
-if ((Get-ItemProperty $RegKey).PSObject.Properties.Name -contains $StatusMsgProperty) {
-    try {
-        Set-Itemproperty -path $RegKey -Name $StatusMsgProperty -value '1'
-    }
-    catch {
-        $_.Exception.Message
+    else {
+        try {
+            New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type
+        }
+        catch {
+            Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
+            Write-Verbose $Error[0]
+            Exit 1
+        }
     }
 }
-else {
-    try {
-        New-ItemProperty -path $RegKey -Name $StatusMsgProperty -value '1' -Type DWORD
-    }
-    catch {
-        $_.Exception.Message
-    }
-
-}
-Exit $LASTEXITCODE
+Exit 0
