1cd279f84d6f9dcd2526788d616001fc4244fb07
Darnel Kumar <darnel.kumar@dgk.one>
2022-12-30 17:27:38 +0000

    Update Disable-VerboseStatusMessages.ps1

--- Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
+++ Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\PowerShell Scripts\Disable-VerboseStatusMessages.ps1
  * Repository: Public
  * Created Date: Wednesday, November 9th 2022, 9:20:26 AM
- * Last Modified: Friday, December 30th 2022, 5:06:33 PM
+ * Last Modified: Friday, December 30th 2022, 5:24:56 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -26,6 +26,16 @@ $RegData = @(
 )
 
 foreach ($i in $RegData) {
+    if (!(Test-Path -Path $i.Path)) {
+        try {
+            New-Item -Path $i.Path -Force
+        }
+        catch {
+            Write-Warning "Failed to create registry path: $($i.Path)"
+            Write-Verbose $Error[0]
+            Exit 1
+        }
+    }
     if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
         try {
             Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
