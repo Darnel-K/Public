5b24ab1f93d05f23f6c529915f86b4c1b0b03c6e
Darnel Kumar <darnel.kumar@dgk.one>
2023-01-31 12:02:07 +0000

    Update Install.ps1

--- Intune/Win32 Apps/365 Powershell Modules/Install.ps1
+++ Intune/Win32 Apps/365 Powershell Modules/Install.ps1
@@ -1,94 +1,33 @@
-﻿<#
+<#
  * ############################################################################
  * Filename: \Intune\Win32 Apps\365 Powershell Modules\Install.ps1
  * Repository: Public
- * Created Date: Wednesday, November 9th 2022, 10:14:30 AM
- * Last Modified: Friday, December 9th 2022, 4:34:35 PM
+ * Created Date: Tuesday, January 31st 2023, 10:32:14 AM
+ * Last Modified: Tuesday, January 31st 2023, 11:55:07 AM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
- * Copyright (c) 2022 Darnel Kumar
+ * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
-$Services = ("AzureAD", "MSOnline", "ExchangeOnline", 'SharePoint', 'SharePointPnP', 'SecAndCompCenter', 'Teams')
-Foreach ($Service in $Services) {
-    Write-Host Checking install status for $Service...
-    Switch ($Service) {
-        ExchangeOnline {
-            $Module = Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3 -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host Exchange Online'(EXO V2)' module is not installed  -ForegroundColor yellow
-                Install-Module ExchangeOnlineManagement -Force -Scope AllUsers
-            }
-            else {
-                Write-Host Exchange Online'(EXO V2)' module is installed  -ForegroundColor green
-            }
-        }
+$PSModules = @("PnP.PowerShell", "AzureADPreview", "ExchangeOnlineManagement", "Microsoft.Online.SharePoint.PowerShell", "MicrosoftTeams")
 
-        MSOnline {
-            $Module = Get-Module -Name MSOnline -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host MSOnline module is not installed  -ForegroundColor yellow
-                Install-Module MSOnline -Force -Scope AllUsers
-            }
-            else {
-                Write-Host MSOnline module is installed  -ForegroundColor green
-            }
+foreach ($item in $PSModules) {
+    $Installed = Get-InstalledModule -Name $item -ErrorAction SilentlyContinue
+    if ($Installed) {
+        Write-Host "[INSTALLED] - $item" -ForegroundColor Green
+    }
+    else {
+        Write-Host "[ MISSING ] - $item" -ForegroundColor Red
+        Write-Host "[INSTALLING] - $item" -ForegroundColor Yellow
+        Install-Module -Name $item -Force -AllowClobber -Scope AllUsers
+        if ((Get-InstalledModule -Name $item -ErrorAction SilentlyContinue)) {
+            Write-Host "[INSTALLED] - $item" -ForegroundColor Green
         }
-
-        AzureAD {
-            $Module = Get-Module -Name AzureAD -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host AzureAD module is not installed  -ForegroundColor yellow
-                Install-Module AzureAD -Force -Scope AllUsers
-            }
-            else {
-                Write-Host AzureAD module is installed  -ForegroundColor green
-            }
-        }
-
-        SharePoint {
-            $Module = Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host SharePoint Online PowerShell module is not installed  -ForegroundColor yellow
-                Install-Module Microsoft.Online.SharePoint.PowerShell -Force -Scope AllUsers
-            }
-            else {
-                Write-Host SharePoint Online PowerShell module is installed  -ForegroundColor green
-            }
-        }
-
-        SharePointPnP {
-            $Module = Get-InstalledModule -Name SharePointPnPPowerShellOnline -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host SharePoint PnP module module is not installed  -ForegroundColor yellow
-                Install-Module -Name SharePointPnPPowerShellOnline -AllowClobber -Force -Scope AllUsers
-            }
-            else {
-                Write-Host SharePoint PnP module module is installed  -ForegroundColor green
-            }
-        }
-
-        SecAndCompCenter {
-            $Module = Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3 -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host Exchange Online'(EXO V2)' module is not installed  -ForegroundColor yellow
-                Install-Module ExchangeOnlineManagement -Force -Scope AllUsers
-            }
-            else {
-                Write-Host Exchange Online'(EXO V2)' module is installed  -ForegroundColor green
-            }
-        }
-
-        Teams {
-            $Module = Get-InstalledModule -Name MicrosoftTeams -MinimumVersion 4.0.0 -ErrorAction SilentlyContinue
-            if ($Module.count -eq 0) {
-                Write-Host MicrosoftTeams module is not installed  -ForegroundColor yellow
-                Install-Module MicrosoftTeams -AllowClobber -Force -Scope AllUsers
-            }
-            else {
-                Write-Host MicrosoftTeams module is installed  -ForegroundColor green
-            }
+        else {
+            Write-Host "[ FAILED ] - $item" -ForegroundColor Red
         }
     }
 }
+Write-Host "All Modules Installed!" -ForegroundColor Green
+Exit 0
