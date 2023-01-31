9353644828615a1657c3e418845e8e2d78233c2e
Darnel Kumar <darnel.kumar@dgk.one>
2023-01-31 14:55:08 +0000

    Update Install.ps1

--- Intune/Win32 Apps/365 Powershell Modules/Install.ps1
+++ Intune/Win32 Apps/365 Powershell Modules/Install.ps1
@@ -3,15 +3,32 @@
  * Filename: \Intune\Win32 Apps\365 Powershell Modules\Install.ps1
  * Repository: Public
  * Created Date: Tuesday, January 31st 2023, 10:32:14 AM
- * Last Modified: Tuesday, January 31st 2023, 11:55:07 AM
+ * Last Modified: Tuesday, January 31st 2023, 2:52:33 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
+Start-Transcript -Path "C:\MS365PowerShellModules.log" -Force
 $PSModules = @("PnP.PowerShell", "AzureADPreview", "ExchangeOnlineManagement", "Microsoft.Online.SharePoint.PowerShell", "MicrosoftTeams")
+# Check for NuGet package provider
+if ((Get-PackageProvider -Name NuGet)) {
+    Write-Host "[INSTALLED] - NuGet" -ForegroundColor Green
+}
+else {
+    Write-Host "[ MISSING ] - NuGet" -ForegroundColor Red
+    Write-Host "[INSTALLING] - NuGet" -ForegroundColor Yellow
+    Install-PackageProvider -Name NuGet -Force -Verbose:$false -Scope AllUsers -Confirm:$false
+    if ((Get-PackageProvider -Name NuGet)) {
+        Write-Host "[INSTALLED] - NuGet" -ForegroundColor Green
+    }
+    else {
+        Write-Host "[ FAILED ] - NuGet" -ForegroundColor Red
+    }
+}
 
+# Check for and install each required module
 foreach ($item in $PSModules) {
     $Installed = Get-InstalledModule -Name $item -ErrorAction SilentlyContinue
     if ($Installed) {
@@ -20,7 +37,7 @@ foreach ($item in $PSModules) {
     else {
         Write-Host "[ MISSING ] - $item" -ForegroundColor Red
         Write-Host "[INSTALLING] - $item" -ForegroundColor Yellow
-        Install-Module -Name $item -Force -AllowClobber -Scope AllUsers
+        Install-Module -Name $item -Scope AllUsers -Force -Confirm:$false -Verbose:$false -AllowClobber
         if ((Get-InstalledModule -Name $item -ErrorAction SilentlyContinue)) {
             Write-Host "[INSTALLED] - $item" -ForegroundColor Green
         }
@@ -30,4 +47,5 @@ foreach ($item in $PSModules) {
     }
 }
 Write-Host "All Modules Installed!" -ForegroundColor Green
+Stop-Transcript
 Exit 0
