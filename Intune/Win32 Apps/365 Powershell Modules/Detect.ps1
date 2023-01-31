4c8a721ae6377f15b95af384e6565c4ac63dd52f
Darnel Kumar <darnel.kumar@dgk.one>
2023-01-31 12:02:04 +0000

    Update Detect.ps1

--- Intune/Win32 Apps/365 Powershell Modules/Detect.ps1
+++ Intune/Win32 Apps/365 Powershell Modules/Detect.ps1
@@ -1,103 +1,26 @@
-﻿<#
+<#
  * ############################################################################
  * Filename: \Intune\Win32 Apps\365 Powershell Modules\Detect.ps1
  * Repository: Public
- * Created Date: Wednesday, November 9th 2022, 10:14:30 AM
- * Last Modified: Wednesday, November 9th 2022, 2:12:44 PM
+ * Created Date: Tuesday, January 31st 2023, 10:32:14 AM
+ * Last Modified: Tuesday, January 31st 2023, 11:46:53 AM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
- * Copyright (c) 2022 Darnel Kumar
+ * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
-$Services = ("AzureAD", "MSOnline", "ExchangeOnline", 'SharePoint', 'SharePointPnP', 'SecAndCompCenter', 'Teams')
-Foreach ($Service in $Services) {
-    Write-Host Checking install status for $Service...
-    Switch ($Service) {
-        ExchangeOnline {
-            $Module = Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3 -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host Exchange Online'(EXO V2)' module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host Exchange Online'(EXO V2)' module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
+$PSModules = @("PnP.PowerShell", "AzureADPreview", "ExchangeOnlineManagement", "Microsoft.Online.SharePoint.PowerShell", "MicrosoftTeams")
 
-        MSOnline {
-            $Module = Get-Module -Name MSOnline -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host MSOnline module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host MSOnline module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
-
-        AzureAD {
-            $Module = Get-Module -Name AzureAD -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host AzureAD module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host AzureAD module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
-
-        SharePoint {
-            $Module = Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host SharePoint Online PowerShell module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host SharePoint Online PowerShell module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
-
-        SharePointPnP {
-            $Module = Get-InstalledModule -Name SharePointPnPPowerShellOnline -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host SharePoint PnP module module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host SharePoint PnP module module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
-
-        SecAndCompCenter {
-            $Module = Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3 -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host Exchange Online'(EXO V2)' module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host Exchange Online'(EXO V2)' module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
-
-        Teams {
-            $Module = Get-InstalledModule -Name MicrosoftTeams -MinimumVersion 4.0.0 -ErrorAction SilentlyContinue
-            if ($Module.count -ne 0) {
-                Write-Host MicrosoftTeams module is installed!  -ForegroundColor green
-                Continue
-            }
-            else {
-                Write-Host MicrosoftTeams module is not installed  -ForegroundColor red
-                Exit 1
-            }
-        }
+foreach ($item in $PSModules) {
+    $Installed = Get-InstalledModule -Name $item -ErrorAction SilentlyContinue
+    if ($Installed) {
+        Write-Host "[INSTALLED] - $item" -ForegroundColor Green
+    }
+    else {
+        Write-Host "[ MISSING ] - $item" -ForegroundColor Red
+        Exit 1
     }
 }
-Write-Host All Installed! -ForegroundColor green
+Write-Host "All Modules Installed!" -ForegroundColor Green
 Exit 0
