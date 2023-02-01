bb4575341e3147bdd113f127a8ef296ab1ee2a16
Darnel Kumar <darnel.kumar@dgk.one>
2023-02-01 09:26:05 +0000

    Update Uninstall.ps1

--- Intune/Win32 Apps/365 Powershell Modules/Uninstall.ps1
+++ Intune/Win32 Apps/365 Powershell Modules/Uninstall.ps1
@@ -3,13 +3,14 @@
  * Filename: \Intune\Win32 Apps\365 Powershell Modules\Uninstall.ps1
  * Repository: Public
  * Created Date: Tuesday, January 31st 2023, 10:32:14 AM
- * Last Modified: Tuesday, January 31st 2023, 2:46:22 PM
+ * Last Modified: Wednesday, February 1st 2023, 9:25:53 AM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
+Start-Transcript -Path "$env:USERPROFILE\Logs\Uninstall365PowerShellModules.Intune.log" -Force
 $PSModules = @("PnP.PowerShell", "AzureADPreview", "ExchangeOnlineManagement", "Microsoft.Online.SharePoint.PowerShell", "MicrosoftTeams")
 
 foreach ($item in $PSModules) {
@@ -31,4 +32,5 @@ foreach ($item in $PSModules) {
     }
 }
 Write-Host "All Modules Uninstalled!" -ForegroundColor Green
+Stop-Transcript
 Exit 0
