cf9f9f844b4eb814bcf5f12b8e2d3f028a69cea2
Darnel Kumar <darnel.kumar@dgk.one>
2023-02-01 08:54:58 +0000

    Create Get-OrgHierarchy.ps1

---
+++ PowerShell Scripts/AzureAD/Get-OrgHierarchy.ps1
@@ -0,0 +1,18 @@
+<#
+ * ############################################################################
+ * Filename: \PowerShell Scripts\AzureAD\Get-OrgHierarchy.ps1
+ * Repository: Public
+ * Created Date: Monday, December 5th 2022, 4:20:07 PM
+ * Last Modified: Wednesday, February 1st 2023, 8:54:10 AM
+ * Original Author: Darnel Kumar
+ * Author Github: https://github.com/Darnel-K
+ *
+ * Copyright (c) 2022 Darnel Kumar
+ * ############################################################################
+#>
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [TypeName]
+    $ParameterName
+)
