c169c10a272b611a969982460dd009ac9364a9f1
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-10 17:47:58 +0000

    Update Invoke-SetRegionUK.ps1

--- Intune/PowerShell Scripts/Invoke-SetRegionUK.ps1
+++ Intune/PowerShell Scripts/Invoke-SetRegionUK.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\PowerShell Scripts\Invoke-SetRegionUK.ps1
  * Repository: Public
  * Created Date: Friday, March 10th 2023, 4:23:48 PM
- * Last Modified: Friday, March 10th 2023, 5:47:32 PM
+ * Last Modified: Friday, March 10th 2023, 5:47:57 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -12,6 +12,7 @@
 #>
 
 # https://superuser.com/questions/1579847/what-do-i-have-to-specify-to-make-set-culture-set-a-default-of-24-hours-rather-t
+# https://stackoverflow.com/questions/28749439/changing-windows-time-and-date-format
 $DesiredLanguage = "en-GB"
 $LanguageFeatures = (Get-InstalledLanguage -Language $DesiredLanguage).LanguageFeatures
 if (($null -eq $LanguageFeatures) -or ($LanguageFeatures -eq "None")) {
