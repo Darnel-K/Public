e74741e74e00ddffe75131c2d0ffa00491d4b71e
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-10 16:37:20 +0000

    Create Invoke-SetRegionUK.ps1

---
+++ Intune/PowerShell Scripts/Invoke-SetRegionUK.ps1
@@ -0,0 +1,12 @@
+<#
+ * ############################################################################
+ * Filename: \Intune\PowerShell Scripts\Invoke-SetRegionUK.ps1
+ * Repository: Public
+ * Created Date: Friday, March 10th 2023, 4:23:48 PM
+ * Last Modified: Friday, March 10th 2023, 4:24:04 PM
+ * Original Author: Darnel Kumar
+ * Author Github: https://github.com/Darnel-K
+ *
+ * Copyright (c) 2023 Darnel Kumar
+ * ############################################################################
+#>
