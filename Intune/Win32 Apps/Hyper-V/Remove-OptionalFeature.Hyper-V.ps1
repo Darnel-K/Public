0037a113fd13d76b6b1153626c3254bf297f8f8f
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-10 15:40:31 +0000

    Update Remove-OptionalFeature.Hyper-V.ps1

--- Intune/Win32 Apps/Hyper-V/Remove-OptionalFeature.Hyper-V.ps1
+++ Intune/Win32 Apps/Hyper-V/Remove-OptionalFeature.Hyper-V.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\Win32 Apps\Hyper-V\Remove-OptionalFeature.Hyper-V.ps1
  * Repository: Public
  * Created Date: Friday, March 10th 2023, 2:01:17 PM
- * Last Modified: Friday, March 10th 2023, 3:10:41 PM
+ * Last Modified: Friday, March 10th 2023, 3:40:18 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -14,7 +14,7 @@ try {
     Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart
     if ( ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Hypervisor).State -eq "Disabled") -and (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Clients).State -eq "Disabled") {
         Write-Host "[  MISSING  ] - Hyper-V" -ForegroundColor Green
-        Exit 0
+        Exit 3010
     }
     else {
         Write-Host "[  FAILED  ] - Hyper-V" -ForegroundColor Red
