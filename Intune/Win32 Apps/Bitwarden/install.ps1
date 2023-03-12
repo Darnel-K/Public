0fcc1b6d3ef91739c677cc522a1b1f61a2ff50b1
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-12 15:33:25 +0000

    Update install.ps1

--- Intune/Win32 Apps/Bitwarden/install.ps1
+++ Intune/Win32 Apps/Bitwarden/install.ps1
@@ -2,24 +2,57 @@
  * ############################################################################
  * Filename: \Intune\Win32 Apps\Bitwarden\install.ps1
  * Repository: Public
- * Created Date: Saturday, March 11th 2023, 7:20:57 PM
- * Last Modified: Sunday, March 12th 2023, 1:57:46 PM
+ * Created Date: Sunday, March 12th 2023, 2:18:06 PM
+ * Last Modified: Sunday, March 12th 2023, 3:33:14 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
-$LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.Win32App.Bitwarden";
-if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
-    New-EventLog -LogName $LogName -Source $LogSource
-    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
+<#
+.SYNOPSIS
+    Installs Bitwarden
+.DESCRIPTION
+    Script for an Intune Win32 to install bitwarden silently
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+# [CmdletBinding()]
+# Param (
+#     # Param1 help description
+#     [Parameter(ValueFromPipeline = $true)]
+#     [string]
+#     $Var1
+# )
+begin {
+    # Update LogName and LogSource
+    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.Win32App.Bitwarden";
+    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
+        New-EventLog -LogName $LogName -Source $LogSource
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
+    }
+    # Add any code here that needs be done once during the initialisation phase
+    # e.g. database connections, variable declarations, Event Log checks
 }
 
-try {
-    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Starting Bitwarden installation" -EventId 0
-    # Start-Process -FilePath ".\Bitwarden-Installer-2023.2.0.exe" -Wait -WindowStyle Hidden -ArgumentList "/S" -Verb RunAs
+process {
+    try {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Starting Bitwarden installation" -EventId 0
+        # Start-Process -FilePath ".\Bitwarden-Installer-2023.2.0.exe" -Wait -WindowStyle Hidden -ArgumentList "/S" -Verb RunAs
+    }
+    catch {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0]. -EventId 0
+    }
+
 }
-catch {
-    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0]. -EventId 0
+
+end {
+    # Add code here that should be run once the process block is complete.
+    # E.g. closing database connections, outputing results
 }
