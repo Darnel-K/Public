345f157d4b8cdea8afc8ee1fe073402a755fab56
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-12 13:57:59 +0000

    Update install.ps1

--- Intune/Win32 Apps/Bitwarden/install.ps1
+++ Intune/Win32 Apps/Bitwarden/install.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\Win32 Apps\Bitwarden\install.ps1
  * Repository: Public
  * Created Date: Saturday, March 11th 2023, 7:20:57 PM
- * Last Modified: Sunday, March 12th 2023, 1:25:42 PM
+ * Last Modified: Sunday, March 12th 2023, 1:57:46 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -13,7 +13,13 @@
 $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.Win32App.Bitwarden";
 if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
     New-EventLog -LogName $LogName -Source $LogSource
-    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 1
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
 }
 
-# Start-Process -FilePath ".\Bitwarden-Installer-2023.2.0.exe" -Wait -WindowStyle Hidden -ArgumentList "/S" -Verb RunAs
+try {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Starting Bitwarden installation" -EventId 0
+    # Start-Process -FilePath ".\Bitwarden-Installer-2023.2.0.exe" -Wait -WindowStyle Hidden -ArgumentList "/S" -Verb RunAs
+}
+catch {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0]. -EventId 0
+}
