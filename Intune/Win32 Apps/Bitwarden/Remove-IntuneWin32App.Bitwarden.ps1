891358d0701800b356e10fda3208b4dc2cccd5e4
Darnel Kumar <darnel.kumar@dgk.one>
2023-03-15 16:43:52 +0000

    Update Remove-IntuneWin32App.Bitwarden.ps1

--- Intune/Win32 Apps/Bitwarden/Remove-IntuneWin32App.Bitwarden.ps1
+++ Intune/Win32 Apps/Bitwarden/Remove-IntuneWin32App.Bitwarden.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\Win32 Apps\Bitwarden\Remove-IntuneWin32App.Bitwarden.ps1
  * Repository: Public
  * Created Date: Sunday, March 12th 2023, 2:18:06 PM
- * Last Modified: Sunday, March 12th 2023, 10:59:14 PM
+ * Last Modified: Wednesday, March 15th 2023, 4:42:35 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -35,8 +35,8 @@ begin {
         catch {
             $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
             $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Message -EventId 1000
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 1000
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 1000
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 1000
         }
     }
     if (-not (Test-Path "$env:ProgramFiles\Bitwarden\Bitwarden.exe" -PathType Leaf)) {
@@ -49,8 +49,10 @@ process {
     try {
         Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Starting Bitwarden uninstallation" -EventId 0
         Start-Process -FilePath $env:ProgramFiles"\Bitwarden\Uninstall Bitwarden.exe" -Wait -WindowStyle Hidden -ArgumentList "/S /ALLUSERS" -Verb RunAs
-        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Bitwarden uninstallation complete" -EventId 0
-        Exit 0
+        if (-not (Test-Path "$env:ProgramFiles\Bitwarden\Bitwarden.exe" -PathType Leaf)) {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Bitwarden uninstallation complete" -EventId 0
+            Exit 0
+        }
     }
     catch {
         Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
