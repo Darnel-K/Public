a06058b4919a72da241b9922682b44e53ce24d01
Darnel Kumar <darnel.kumar@dgk.one>
2023-04-13 17:03:19 +0100

    Update Set-UKLocale.ps1

--- Intune/PowerShell Scripts/Set-UKLocale.ps1
+++ Intune/PowerShell Scripts/Set-UKLocale.ps1
@@ -3,7 +3,7 @@
  * Filename: \Intune\PowerShell Scripts\Set-UKLocale.ps1
  * Repository: Public
  * Created Date: Monday, March 13th 2023, 5:24:01 PM
- * Last Modified: Thursday, April 13th 2023, 11:49:46 AM
+ * Last Modified: Thursday, April 13th 2023, 2:58:19 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -29,6 +29,8 @@ Param ()
 $ProgressPreference = "Continue"
 $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
 $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.SetUKLocale";
+$DesiredLanguage = "en-GB"
+$PackageName = "Microsoft.LanguageExperiencePacken-GB"
 
 
 if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
@@ -43,5 +45,53 @@ if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Dia
         Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 1000
     }
 }
-# Add any code here that needs be done once during the initialisation phase
-# e.g. database connections, variable declarations, Event Log checks
+
+# Check if Language Pack is installed and install if not
+Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Checking if $DesiredLanguage Language Pack is installed" -EventId 0
+if (-not (Get-WinUserLanguageList | Where-Object { $_.LanguageTag -eq $DesiredLanguage })) {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "$DesiredLanguage Language Pack not installed" -EventId 0
+    try {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Attempting to install $DesiredLanguage Language Pack" -EventId 0
+        Install-Language $DesiredLanguage -CopyToSettings
+        if (Get-WinUserLanguageList | Where-Object { $_.LanguageTag -eq $DesiredLanguage }) {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "$DesiredLanguage Language Pack installed successfully" -EventId 0
+        }
+        else {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message "Unable to install $DesiredLanguage Language Pack" -EventId 1001
+            Exit 1
+        }
+    }
+    catch {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message "Unable to install $DesiredLanguage Language Pack" -EventId 1001
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 1001
+        Exit 1
+    }
+}
+else {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "$DesiredLanguage Language Pack already installed" -EventId 0
+}
+
+# Check if SystemPreferredUILanguage is set to the Desired Language
+Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Checking if SystemPreferredUILanguage is set to $DesiredLanguage" -EventId 0
+if (-not ((Get-SystemPreferredUILanguage) -eq $DesiredLanguage)) {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "SystemPreferredUILanguage not set to $DesiredLanguage" -EventId 0
+    try {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Attempting to set SystemPreferredUILanguage to $DesiredLanguage" -EventId 0
+        Set-SystemPreferredUILanguage $DesiredLanguage
+        if ((Get-SystemPreferredUILanguage) -eq $DesiredLanguage) {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "SystemPreferredUILanguage set to $DesiredLanguage successfully" -EventId 0
+        }
+        else {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message "Unable to set SystemPreferredUILanguage to $DesiredLanguage" -EventId 1002
+            Exit 1
+        }
+    }
+    catch {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message "Unable to set SystemPreferredUILanguage to $DesiredLanguage" -EventId 1002
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 1001
+        Exit 1
+    }
+}
+else {
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "SystemPreferredUILanguage already set to $DesiredLanguage" -EventId 0
+}
