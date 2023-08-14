7831b3d4550962f3df42f121f2e1f57ca7fc2f42
Darnel Kumar <darnel.kumar@dgk.one>
2023-08-14 14:19:16 +0100

    Update Template.ps1

--- PowerShell Scripts/Template.ps1
+++ PowerShell Scripts/Template.ps1
@@ -2,8 +2,8 @@
  * ############################################################################
  * Filename: \PowerShell Scripts\Template.ps1
  * Repository: Public
- * Created Date: Monday, March 13th 2023, 5:24:01 PM
- * Last Modified: Monday, August 14th 2023, 12:27:57 PM
+ * Created Date: Monday, August 14th 2023, 12:26:40 PM
+ * Last Modified: Monday, August 14th 2023, 2:19:03 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
@@ -51,13 +51,13 @@ begin {
     if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
         try {
             New-EventLog -LogName $LogName -Source $LogSource
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 1
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
         }
         catch {
             $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
             $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 1000
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 1000
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
         }
     }
     # Add any code here that needs be done once during the initialisation phase
