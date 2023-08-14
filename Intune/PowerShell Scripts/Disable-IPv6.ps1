08b8c2cd2ff5679765a9408f33eb80920a1eeb14
Darnel Kumar <darnel.kumar@dgk.one>
2023-08-14 12:38:13 +0100

    Update Disable-IPv6.ps1

--- Intune/PowerShell Scripts/Disable-IPv6.ps1
+++ Intune/PowerShell Scripts/Disable-IPv6.ps1
@@ -2,12 +2,42 @@
  * ############################################################################
  * Filename: \Intune\PowerShell Scripts\Disable-IPv6.ps1
  * Repository: Public
- * Created Date: Monday, May 15th 2023, 10:15:02 PM
- * Last Modified: Monday, May 15th 2023, 10:18:02 PM
+ * Created Date: Wednesday, June 14th 2023, 9:52:14 AM
+ * Last Modified: Monday, August 14th 2023, 12:37:40 PM
  * Original Author: Darnel Kumar
  * Author Github: https://github.com/Darnel-K
  *
  * Copyright (c) 2023 Darnel Kumar
  * ############################################################################
 #>
-Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+
+<#
+.SYNOPSIS
+    Disables IPv6
+.DESCRIPTION
+    Disables IPv6 on the executing device
+.EXAMPLE
+    & .\Disable-IPv6.ps1
+#>
+begin {
+    $ProgressPreference = "Continue"
+    $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+    # Update LogName and LogSource
+    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.Disable-IPv6";
+    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
+        try {
+            New-EventLog -LogName $LogName -Source $LogSource
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
+        }
+        catch {
+            $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
+            $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
+        }
+    }
+}
+
+process {
+    Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+}
