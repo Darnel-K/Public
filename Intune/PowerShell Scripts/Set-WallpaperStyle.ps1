11b14f3313738d090663f8b490ef27a59bb1b841
Darnel Kumar <darnel.kumar@dgk.one>
2023-11-02 11:56:25 +0000

    Update Set-WallpaperStyle.ps1

--- Intune/PowerShell Scripts/Set-WallpaperStyle.ps1
+++ Intune/PowerShell Scripts/Set-WallpaperStyle.ps1
@@ -1,32 +1,17 @@
 <#
- * ############################################################################
- * Filename: \Intune\PowerShell Scripts\Set-WallpaperStyle.ps1
- * Repository: Public
- * Created Date: Monday, May 15th 2023, 8:37:17 PM
- * Last Modified: Monday, May 15th 2023, 10:04:34 PM
- * Original Author: Darnel Kumar
- * Author Github: https://github.com/Darnel-K
- *
- * Copyright (c) 2023 Darnel Kumar
- * ############################################################################
+# ############################################################################ #
+# Filename: \Intune\PowerShell Scripts\Set-WallpaperStyle.ps1                  #
+# Repository: Public                                                           #
+# Created Date: Wednesday, June 14th 2023, 9:52:14 AM                          #
+# Last Modified: Thursday, November 2nd 2023, 11:56:13 AM                      #
+# Original Author: Darnel Kumar                                                #
+# Author Github: https://github.com/Darnel-K                                   #
+#                                                                              #
+# Copyright (c) 2023 Darnel Kumar                                              #
+# ############################################################################ #
 #>
 
 $RegKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
-
-if ( !(Test-Path -Path $filename) ) {
-    $bytes = [Convert]::FromBase64String($b64)
-    [IO.File]::WriteAllBytes($filename, $bytes)
-    if ( Test-Path -Path $filename) {
-        Write-Output "File created successfully"
-    }
-    else {
-        Write-Output "Failed to create file"
-        Exit 1
-    }
-}
-else {
-    Write-Output "File already exists"
-}
 
 $RegData = @(
     [PSCustomObject]@{
