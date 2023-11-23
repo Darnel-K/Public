dd11a214c1a3aa486b133a72854778bc671e463d
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2023-11-23 16:23:21 +0000

    Update Enable-WOL.ps1

--- Intune/PowerShell Scripts/Enable-WOL.ps1
+++ Intune/PowerShell Scripts/Enable-WOL.ps1
@@ -1,21 +1,70 @@
 <#
- * ############################################################################
- * Filename: \Intune\PowerShell Scripts\Enable-WOL.ps1
- * Repository: Public
- * Created Date: Thursday, June 1st 2023, 2:47:39 PM
- * Last Modified: Thursday, June 1st 2023, 4:48:04 PM
- * Original Author: Darnel Kumar
- * Author Github: https://github.com/Darnel-K
- *
- * Copyright (c) 2023 Darnel Kumar
- * ############################################################################
+# ############################################################################ #
+# Filename: \Intune\PowerShell Scripts\Enable-WOL.ps1                          #
+# Repository: Public                                                           #
+# Created Date: Friday, June 2nd 2023, 5:22:51 PM                              #
+# Last Modified: Thursday, November 23rd 2023, 4:20:29 PM                      #
+# Original Author: Darnel Kumar                                                #
+# Author Github: https://github.com/Darnel-K                                   #
+#                                                                              #
+# Copyright (c) 2023 Darnel Kumar                                              #
+# ############################################################################ #
 #>
 
-$NetAdapters = Get-NetAdapter | Where-Object { ($_.Name -like "*Ethernet*") -or ($_.Name -like "*WiFi*") }
-
-foreach ($item in $NetAdapters) {
-    powercfg.exe /deviceenablewake "$($item.InterfaceDescription)"
+<#
+.SYNOPSIS
+    Enables WOL
+.DESCRIPTION
+    Enables Wake On LAN on the executing device
+.EXAMPLE
+    & .\Enable-WOL.ps1
+#>
+begin {
+    $ProgressPreference = "Continue"
+    $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+    # Update LogName and LogSource
+    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.Enable-WOL";
+    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
+        try {
+            New-EventLog -LogName $LogName -Source $LogSource
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
+        }
+        catch {
+            $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
+            $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
+        }
+    }
+    $networkAdapters = @()
+    $Errors = 0
 }
 
-Enable-NetAdapterPowerManagement -Name * -WakeOnMagicPacket -WakeOnPattern
-Exit 0
+process {
+    $networkAdapters += Get-NetAdapterPowerManagement | Where-Object { (($_.Name -like "*Ethernet*") -or ($_.Name -like "*WiFi*")) -and (($_.WakeOnMagicPacket -eq "Disabled") -or ($_.WakeOnPattern -eq "Disabled")) }
+    foreach ($item in $networkAdapters) {
+        try {
+            powercfg.exe /deviceenablewake "$($item.InterfaceDescription)"
+            Enable-NetAdapterPowerManagement -InputObject $item -WakeOnMagicPacket -WakeOnPattern
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Enabled WOL on '$($item.Name)' - '$($item.InterfaceDescription)'." -EventId 0
+        }
+        catch {
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message "Unable to enable WOL on '$($item.Name)' - '$($item.InterfaceDescription)'." -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+            $Errors++
+        }
+    }
+}
+
+end {
+    $failedAdapters = @()
+    $failedAdapters += Get-NetAdapterPowerManagement | Where-Object { (($_.Name -like "*Ethernet*") -or ($_.Name -like "*WiFi*")) -and (($_.WakeOnMagicPacket -eq "Disabled") -or ($_.WakeOnPattern -eq "Disabled")) }
+    if (($failedAdapters.Count -eq 0) -and ($Errors -eq 0)) {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Enabled WOL on all network devices." -EventId 0
+        Exit 0
+    }
+    else {
+        Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message "Unable to enable WOL on all devices. Check Event Log for more info." -EventId 0
+        Exit 1
+    }
+}
