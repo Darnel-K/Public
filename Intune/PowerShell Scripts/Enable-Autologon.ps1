62078dc041f20acfa3b069c8ea9e5d48a5e7975b
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2023-11-23 16:23:12 +0000

    Update Enable-Autologon.ps1

--- Intune/PowerShell Scripts/Enable-Autologon.ps1
+++ Intune/PowerShell Scripts/Enable-Autologon.ps1
@@ -3,7 +3,7 @@
 # Filename: \Intune\PowerShell Scripts\Enable-Autologon.ps1                    #
 # Repository: Public                                                           #
 # Created Date: Tuesday, October 17th 2023, 12:24:58 AM                        #
-# Last Modified: Tuesday, October 17th 2023, 12:29:55 AM                       #
+# Last Modified: Thursday, November 23rd 2023, 3:36:03 PM                      #
 # Original Author: Darnel Kumar                                                #
 # Author Github: https://github.com/Darnel-K                                   #
 #                                                                              #
@@ -11,58 +11,86 @@
 # ############################################################################ #
 #>
 
-$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$RegData = @(
-    [PSCustomObject]@{
-        Path  = $RegKeyPath
-        Name  = "AutoAdminLogon"
-        Value = "1"
-        Type  = "STRING"
-    }
-    [PSCustomObject]@{
-        Path  = $RegKeyPath
-        Name  = "DefaultUserName"
-        Value = "KioskUser0"
-        Type  = "STRING"
-    }
-    [PSCustomObject]@{
-        Path  = $RegKeyPath
-        Name  = "IsConnectedAutoLogon"
-        Value = "0"
-        Type  = "DWord"
-    }
-)
-
-foreach ($i in $RegData) {
-    if (!(Test-Path -Path $i.Path)) {
+<#
+.SYNOPSIS
+    Enables Auto Logon
+.DESCRIPTION
+    Enables Auto Logon on the executing device
+.EXAMPLE
+    & .\Enable-Autologon.ps1
+#>
+begin {
+    $ProgressPreference = "Continue"
+    $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+    # Update LogName and LogSource
+    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.Enable-Autologon";
+    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not ([System.Diagnostics.EventLog]::SourceExists($LogSource))) {
         try {
-            New-Item -Path $i.Path -Force
+            New-EventLog -LogName $LogName -Source $LogSource
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
         }
         catch {
-            Write-Warning "Failed to create registry path: $($i.Path)"
-            Write-Verbose $Error[0]
-            Exit 1
+            $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
+            $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
         }
     }
-    if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
-        try {
-            Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
+    $RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+    $RegData = @(
+        [PSCustomObject]@{
+            Path  = $RegKeyPath
+            Name  = "AutoAdminLogon"
+            Value = "1"
+            Type  = "STRING"
         }
-        catch {
-            Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
-            Write-Verbose $Error[0]
-            Exit 1
+        [PSCustomObject]@{
+            Path  = $RegKeyPath
+            Name  = "DefaultUserName"
+            Value = "KioskUser0"
+            Type  = "STRING"
         }
-    }
-    else {
-        try {
-            New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type
+        [PSCustomObject]@{
+            Path  = $RegKeyPath
+            Name  = "IsConnectedAutoLogon"
+            Value = "0"
+            Type  = "DWord"
         }
-        catch {
-            Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
-            Write-Verbose $Error[0]
-            Exit 1
-        }
-    }
+    )
 }
-Exit 0
+
+process {
+    foreach ($i in $RegData) {
+        if (!(Test-Path -Path $i.Path)) {
+            try {
+                New-Item -Path $i.Path -Force
+            }
+            catch {
+                Write-Warning "Failed to create registry path: $($i.Path)"
+                Write-Verbose $Error[0]
+                Exit 1
+            }
+        }
+        if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
+            try {
+                Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
+            }
+            catch {
+                Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
+                Write-Verbose $Error[0]
+                Exit 1
+            }
+        }
+        else {
+            try {
+                New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type
+            }
+            catch {
+                Write-Warning @('Failed to make the following registry edit.', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)")
+                Write-Verbose $Error[0]
+                Exit 1
+            }
+        }
+    }
+    Exit 0
+}
