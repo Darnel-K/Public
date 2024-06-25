ff81f7da2a196c107a1c69541d57960be6560c23
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2024-06-25 10:49:31 +0100

    Create Disable-SetTimeZoneAutomatically.ps1

---
+++ Intune/PowerShell Scripts/Disable-SetTimeZoneAutomatically.ps1
@@ -0,0 +1,90 @@
+<#
+# ############################################################################
+# Filename: \Intune\PowerShell Scripts\Disable-SetTimeZoneAutomatically.ps1
+# Repository: Public
+# Created Date: Tuesday, June 25th 2024, 10:47:53 AM
+# Last Modified: Tuesday, June 25th 2024, 10:48:59 AM
+# Original Author: Darnel Kumar
+# Author Github: https://github.com/Darnel-K
+# Github Org: https://github.com/ABYSS-ORG-UK/
+#
+# This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90
+# Copyright (c) 2024 Darnel Kumar
+# ############################################################################
+#>
+
+<#
+.SYNOPSIS
+    Disables the Set Time Zone Automatically Setting
+.DESCRIPTION
+    Disables the Set Time Zone Automatically Setting on the executing device
+.EXAMPLE
+    & .\Disable-SetTimeZoneAutomatically.ps1
+#>
+begin {
+    $ProgressPreference = "Continue"
+    $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+    # Update LogName and LogSource
+    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.Disable-SetTimeZoneAutomatically";
+    $sourceExists = try { ([System.Diagnostics.EventLog]::SourceExists($LogSource)) } catch { $false }
+    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not $sourceExists ) {
+        try {
+            New-EventLog -LogName $LogName -Source $LogSource
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
+        }
+        catch {
+            $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
+            $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
+            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
+        }
+    }
+    $RegData = @(
+        [PSCustomObject]@{
+            Path  = "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate"
+            Name  = "Start"
+            Value = "4"
+            Type  = "DWord"
+        }
+    )
+}
+
+process {
+    foreach ($i in $RegData) {
+        if (!(Test-Path -Path $i.Path)) {
+            try {
+                New-Item -Path $i.Path -Force
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Created path: $($i.Path)" -EventId 0
+            }
+            catch {
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message "Failed to create registry path: $($i.Path)" -EventId 0
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                Exit 1
+            }
+        }
+        if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
+            try {
+                Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message (@('Successfully made the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") | Out-String) -EventId 0
+            }
+            catch {
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message @('Failed to make the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") -EventId 0
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                Exit 1
+            }
+        }
+        else {
+            try {
+                New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message (@('Created the following registry entry:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") | Out-String) -EventId 0
+            }
+            catch {
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message @('Failed to make the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") -EventId 0
+                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                Exit 1
+            }
+        }
+    }
+    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Completed registry update successfully." -EventId 0
+    Exit 0
+}
