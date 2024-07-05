2e3be68b31d987f061a1f86348d495c4f341517e
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2024-07-05 22:13:39 +0100

    Update Disable-SetTimeZoneAutomatically.ps1

--- Intune/PowerShell Scripts/Disable-SetTimeZoneAutomatically.ps1
+++ Intune/PowerShell Scripts/Disable-SetTimeZoneAutomatically.ps1
@@ -1,16 +1,28 @@
 <#
-# ############################################################################
-# Filename: \Intune\PowerShell Scripts\Disable-SetTimeZoneAutomatically.ps1
-# Repository: Public
-# Created Date: Tuesday, June 25th 2024, 10:47:53 AM
-# Last Modified: Tuesday, June 25th 2024, 10:48:59 AM
-# Original Author: Darnel Kumar
-# Author Github: https://github.com/Darnel-K
-# Github Org: https://github.com/ABYSS-ORG-UK/
-#
-# This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90
-# Copyright (c) 2024 Darnel Kumar
-# ############################################################################
+# #################################################################################################################### #
+# Filename: \Intune\PowerShell Scripts\Disable-SetTimeZoneAutomatically.ps1                                            #
+# Repository: Public                                                                                                   #
+# Created Date: Tuesday, June 25th 2024, 10:47:53 AM                                                                   #
+# Last Modified: Friday, July 5th 2024, 10:08:54 PM                                                                    #
+# Original Author: Darnel Kumar                                                                                        #
+# Author Github: https://github.com/Darnel-K                                                                           #
+# Github Org: https://github.com/ABYSS-ORG-UK/                                                                         #
+#                                                                                                                      #
+# This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90                           #
+#                                                                                                                      #
+# License: GNU General Public License v3.0 only - https://www.gnu.org/licenses/gpl-3.0-standalone.html                 #
+# Copyright (c) 2024 Darnel Kumar                                                                                      #
+#                                                                                                                      #
+# This program is free software: you can redistribute it and/or modify                                                 #
+# it under the terms of the GNU General Public License as published by                                                 #
+# the Free Software Foundation, either version 3 of the License, or                                                    #
+# (at your option) any later version.                                                                                  #
+#                                                                                                                      #
+# This program is distributed in the hope that it will be useful,                                                      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                                       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                        #
+# GNU General Public License for more details.                                                                         #
+# #################################################################################################################### #
 #>
 
 <#
@@ -21,70 +33,268 @@
 .EXAMPLE
     & .\Disable-SetTimeZoneAutomatically.ps1
 #>
-begin {
-    $ProgressPreference = "Continue"
-    $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
-    # Update LogName and LogSource
-    $LogName = "ABYSS.ORG.UK"; $LogSource = ".Intune.PSScript.Disable-SetTimeZoneAutomatically";
-    $sourceExists = try { ([System.Diagnostics.EventLog]::SourceExists($LogSource)) } catch { $false }
-    if (-not ([System.Diagnostics.EventLog]::Exists($LogName)) -or -not $sourceExists ) {
-        try {
-            New-EventLog -LogName $LogName -Source $LogSource
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Initialised Event Log: $LogSource" -EventId 0
-        }
-        catch {
-            $Message = "Unable to initialise event log '$LogName' with source '$LogSource', falling back to event log 'Application' with source 'Application'"
-            $LogName = "Application"; $LogSource = "Application"; # DO NOT CHANGE
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Message -EventId 0
-            Write-EventLog -LogName $LogName -Source $LogSource -EntryType Warning -Message $Error[0] -EventId 0
-        }
-    }
-    $RegData = @(
-        [PSCustomObject]@{
-            Path  = "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate"
-            Name  = "Start"
-            Value = "4"
-            Type  = "DWord"
-        }
-    )
-}
 
-process {
-    foreach ($i in $RegData) {
+[CmdletBinding()]
+
+#################################
+#                               #
+#   USER CHANGEABLE VARIABLES   #
+#                               #
+#################################
+
+$SCRIPT_NAME = "Disable-SetTimeZoneAutomatically"
+$REG_DATA = @(
+    [PSCustomObject]@{
+        Path  = "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate"
+        Name  = "Start"
+        Value = "4"
+        Type  = "DWord"
+    }
+)
+
+################################################
+#                                              #
+#   DO NOT EDIT ANYTHING BELOW THIS MESSAGE!   #
+#                                              #
+################################################
+
+# Script functions
+
+function updateRegistry {
+    foreach ($i in $REG_DATA) {
         if (!(Test-Path -Path $i.Path)) {
             try {
-                New-Item -Path $i.Path -Force
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Created path: $($i.Path)" -EventId 0
+                New-Item -Path $i.Path -Force -ErrorAction Stop | Out-Null
+                $CUSTOM_LOG.Success("Created path: $($i.Path)")
             }
             catch {
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message "Failed to create registry path: $($i.Path)" -EventId 0
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                $CUSTOM_LOG.Fail("Failed to create registry path: $($i.Path)")
+                $CUSTOM_LOG.Error($Error[0])
                 Exit 1
             }
         }
         if ((Get-ItemProperty $i.Path).PSObject.Properties.Name -contains $i.Name) {
             try {
-                Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message (@('Successfully made the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") | Out-String) -EventId 0
+                Set-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Force -ErrorAction Stop | Out-Null
+                $CUSTOM_LOG.Success("Successfully made the following registry edit:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
             }
             catch {
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message @('Failed to make the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") -EventId 0
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                $CUSTOM_LOG.Fail("Failed to make the following registry edit:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
+                $CUSTOM_LOG.Error($Error[0])
                 Exit 1
             }
         }
         else {
             try {
-                New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message (@('Created the following registry entry:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") | Out-String) -EventId 0
+                New-ItemProperty -Path $i.Path -Name $i.Name -Value $i.Value -Type $i.Type -Force -ErrorAction Stop | Out-Null
+                $CUSTOM_LOG.Success("Created the following registry entry:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
             }
             catch {
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message @('Failed to make the following registry edit:', "Key: $($i.Path)", "Property: $($i.Name)", "Value: $($i.Value)", "Type: $($i.Type)") -EventId 0
-                Write-EventLog -LogName $LogName -Source $LogSource -EntryType Error -Message $Error[0] -EventId 0
+                $CUSTOM_LOG.Fail("Failed to make the following registry edit:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
+                $CUSTOM_LOG.Error($Error[0])
                 Exit 1
             }
         }
     }
-    Write-EventLog -LogName $LogName -Source $LogSource -EntryType Information -Message "Completed registry update successfully." -EventId 0
+    $CUSTOM_LOG.Success("Completed registry update successfully.")
     Exit 0
 }
+
+function init {
+    updateRegistry
+}
+
+# Script Variables - DO NOT CHANGE!
+$SCRIPT_NAME = ".Intune.PSScript.$($SCRIPT_NAME.Replace(' ',''))"
+$IS_ADMIN = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# Script & Terminal Preferences - DO NOT CHANGE!
+$ProgressPreference = "Continue"
+$InformationPreference = "Continue"
+$DebugPreference = "SilentlyContinue"
+$ErrorActionPreference = "Continue"
+$VerbosePreference = "SilentlyContinue"
+$WarningPreference = "Continue"
+$host.ui.RawUI.WindowTitle = $SCRIPT_NAME
+
+# Initialise CustomLog class for event log
+$CUSTOM_LOG = [CustomLog]@{log_source = $SCRIPT_NAME }
+$CUSTOM_LOG.InitEventLog()
+
+# Console Signature
+$SCRIPT_FILENAME = $MyInvocation.MyCommand.Name
+function sig {
+    $len = @(($SCRIPT_NAME.Length + 13), ($SCRIPT_FILENAME.Length + 10), 20, 42, 29, 40, 63, 62, 61, 44)
+    $len_max = ($len | Measure-Object -Maximum).Maximum
+    Write-Host "`t####$('#'*$len_max)####`n`t#   $(' '*$len_max)   #`n`t#   Script Name: $($SCRIPT_NAME)$(' '*($len_max-$len[0]))   #`n`t#   Filename: $($SCRIPT_FILENAME)$(' '*($len_max-$len[1]))   #`n`t#   $(' '*$len_max)   #`n`t#   Author: Darnel Kumar$(' '*($len_max-$len[2]))   #`n`t#   Author GitHub: https://github.com/Darnel-K$(' '*($len_max-$len[3]))   #`n`t#   Copyright $([char]0x00A9) $(Get-Date -Format  'yyyy') Darnel Kumar$(' '*($len_max-$len[4]))   #`n`t#   $(' '*$len_max)   #`n`t#   $('-'*$len_max)   #`n`t#   $(' '*$len_max)   #`n`t#   License: GNU General Public License v3.0$(' '*($len_max-$len[5]))   #`n`t#   $(' '*$len_max)   #`n`t#   This program is distributed in the hope that it will be useful,$(' '*($len_max-$len[6]))   #`n`t#   but WITHOUT ANY WARRANTY; without even the implied warranty of$(' '*($len_max-$len[7]))   #`n`t#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the$(' '*($len_max-$len[8]))   #`n`t#   GNU General Public License for more details.$(' '*($len_max-$len[9]))   #`n`t#   $(' '*$len_max)   #`n`t####$('#'*$len_max)####" -ForegroundColor Green
+}
+
+# Define CustomLog class
+class CustomLog {
+    hidden [string] $log_name
+    [string] $log_source
+    hidden [Boolean] $event_log_init
+
+    CustomLog() {
+        $this.log_name = "ABYSS.ORG.UK"
+        $this.event_log_init = $false
+    }
+
+    [void] InitEventLog() {
+        if (-not $this.CheckEventLogExists()) {
+            if (-not $this.CreateEventLog()) {
+                Write-Warning "Unable to initialise event log '$($this.log_name)' with source '$($this.log_source)', falling back to event log 'Application' with source 'Application'"
+                $this.log_name, $this.log_source = "Application", "Application"
+                $this.event_log_init = $true
+            }
+            else {
+                $this.event_log_init = $true
+                $this.Success("Log initialised using event log '$($this.log_name)' with source '$($this.log_source)'")
+            }
+        }
+        else {
+            $this.event_log_init = $true
+            Write-Verbose "Event log '$($this.log_name)' with source '$($this.log_source)' already exists, using existing event log"
+        }
+    }
+
+    [Boolean] CheckEventLogInit($event_log_enabled_per_message = $this.event_log_init) {
+        if ($this.event_log_init -and $event_log_enabled_per_message) {
+            return $true
+        }
+        elseif ($this.event_log_init -and -not $event_log_enabled_per_message) {
+            return $false
+        }
+        elseif (-not $this.event_log_init -and -not $event_log_enabled_per_message) {
+            return $false
+        }
+        else {
+            Write-Warning "Cannot write to event log!"
+            Write-Warning "Event log not initialised, please initialise logging system!"
+            return $false
+        }
+    }
+
+    [Boolean] CheckEventLogExists() {
+        Write-Verbose "Checking if event log '$($this.log_name)' & source '$($this.log_source)' exists"
+        try {
+            if (-not ([System.Diagnostics.EventLog]::Exists($this.log_name)) -or -not ([System.Diagnostics.EventLog]::SourceExists($this.log_source))) {
+                Write-Verbose "Event log '$($this.log_name)' or source '$($this.log_source)' does not exist"
+                return $false
+            }
+            else {
+                return $true
+            }
+        }
+        catch {
+            Write-Verbose "Unable to check if event log '$($this.log_name)' or source '$($this.log_source)' exists"
+            Write-Debug $Error[0]
+            return $false
+        }
+
+    }
+
+    [Boolean] CreateEventLog() {
+        try {
+            Write-Verbose "Attempting to create event log '$($this.log_name)' & source '$($this.log_source)'"
+            New-EventLog -LogName $this.log_name -Source $this.log_source -ErrorAction Stop
+            if ($this.CheckEventLogExists()) {
+                return $true
+            }
+            else {
+                throw "Unable to create event log '$($this.log_name)' or source '$($this.log_source)'"
+            }
+        }
+        catch {
+            Write-Verbose "Unable to create event log '$($this.log_name)' or source '$($this.log_source)'"
+            Write-Debug $Error[0]
+            return $false
+        }
+    }
+
+    [void] Success([string]$msg) {
+        $this.Success($msg, 0, $this.event_log_init)
+    }
+    [void] Success([string]$msg, [Boolean]$event_log_enabled) {
+        $this.Success($msg, 0, $event_log_enabled)
+    }
+    [void] Success([string]$msg, [int]$event_id) {
+        $this.Success($msg, $event_id, $this.event_log_init)
+    }
+    [void] Success([string]$msg, [int]$event_id, [Boolean]$event_log_enabled) {
+        if ($this.CheckEventLogInit($event_log_enabled)) {
+            Write-EventLog -LogName $this.log_name -Source $this.log_source -EntryType SuccessAudit -Message $msg -EventId $event_id
+        }
+        Write-Host "SUCCESS: $msg" -ForegroundColor Green
+    }
+
+    [void] Fail([string]$msg) {
+        $this.Fail($msg, 0, $this.event_log_init)
+    }
+    [void] Fail([string]$msg, [Boolean]$event_log_enabled) {
+        $this.Fail($msg, 0, $event_log_enabled)
+    }
+    [void] Fail([string]$msg, [int]$event_id) {
+        $this.Fail($msg, $event_id, $this.event_log_init)
+    }
+    [void] Fail([string]$msg, [int]$event_id, [Boolean]$event_log_enabled) {
+        if ($this.CheckEventLogInit($event_log_enabled)) {
+            Write-EventLog -LogName $this.log_name -Source $this.log_source -EntryType FailureAudit -Message $msg -EventId $event_id
+        }
+        Write-Host "FAILURE: $msg" -ForegroundColor Red
+    }
+
+    [void] Information([string]$msg) {
+        $this.Information($msg, 0, $this.event_log_init)
+    }
+    [void] Information([string]$msg, [Boolean]$event_log_enabled) {
+        $this.Information($msg, 0, $event_log_enabled)
+    }
+    [void] Information([string]$msg, [int]$event_id) {
+        $this.Information($msg, $event_id, $this.event_log_init)
+    }
+    [void] Information([string]$msg, [int]$event_id, [Boolean]$event_log_enabled) {
+        if ($this.CheckEventLogInit($event_log_enabled)) {
+            Write-EventLog -LogName $this.log_name -Source $this.log_source -EntryType Information -Message $msg -EventId $event_id
+        }
+        Write-Information "INFO: $msg" -InformationAction Continue
+    }
+
+    [void] Warning([string]$msg) {
+        $this.Warning($msg, 0, $this.event_log_init)
+    }
+    [void] Warning([string]$msg, [Boolean]$event_log_enabled) {
+        $this.Warning($msg, 0, $event_log_enabled)
+    }
+    [void] Warning([string]$msg, [int]$event_id) {
+        $this.Warning($msg, $event_id, $this.event_log_init)
+    }
+    [void] Warning([string]$msg, [int]$event_id, [Boolean]$event_log_enabled) {
+        if ($this.CheckEventLogInit($event_log_enabled)) {
+            Write-EventLog -LogName $this.log_name -Source $this.log_source -EntryType Warning -Message $msg -EventId $event_id
+        }
+        Write-Warning $msg -WarningAction Continue
+    }
+
+    [void] Error([string]$msg) {
+        $this.Error($msg, 0, $this.event_log_init)
+    }
+    [void] Error([string]$msg, [Boolean]$event_log_enabled) {
+        $this.Error($msg, 0, $event_log_enabled)
+    }
+    [void] Error([string]$msg, [int]$event_id) {
+        $this.Error($msg, $event_id, $this.event_log_init)
+    }
+    [void] Error([string]$msg, [int]$event_id, [Boolean]$event_log_enabled) {
+        if ($this.CheckEventLogInit($event_log_enabled)) {
+            Write-EventLog -LogName $this.log_name -Source $this.log_source -EntryType Error -Message $msg -EventId $event_id
+        }
+        Write-Error "ERROR: $msg" -ErrorAction Continue
+    }
+
+}
+
+# Clear console & display signature before script initialisation
+Clear-Host
+sig
+init
