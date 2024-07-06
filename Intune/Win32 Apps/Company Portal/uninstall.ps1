9977403f6da7063743aca483a51184e35e64089a
Darnel Kumar <darnel.kumar@dgk.one>
2024-07-06 01:26:52 +0100

    Delete uninstall.ps1

--- Intune/Win32 Apps/Company Portal/uninstall.ps1
+++
@@ -1,2 +0,0 @@
-Get-AppxPackage -AllUsers "*CompanyPortal*" | Remove-AppxPackage
-Get-appxprovisionedpackage –online | where-object { $_.packagename –like "*CompanyPortal*" } | remove-appxprovisionedpackage –online
