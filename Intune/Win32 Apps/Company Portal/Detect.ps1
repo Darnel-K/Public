110d8e47ab45f574ef4abd27725edf4f581bbe1d
Darnel Kumar <darnel.kumar@dgk.one>
2024-07-06 01:26:47 +0100

    Delete Detect.ps1

--- Intune/Win32 Apps/Company Portal/Detect.ps1
+++
@@ -1,9 +0,0 @@
-$app = "Company Portal"
-if (Get-AppxPackage -Name *CompanyPortal* -AllUsers) {
-    Write-Output "$app App Installed"
-    Exit 0
-}
-else {
-    Write-Error "Error: $app Not Installed"
-    Exit 1
-}
