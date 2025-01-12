13e184c8cf4baabba64ca9b8bfa8ed4c6de04c39
Darnel Kumar <darnel.kumar@dgk.one>
2025-01-12 23:00:31 +0000

    Update Disable-VerboseStatusMessages.ps1

--- Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
+++ Intune/PowerShell Scripts/Disable-VerboseStatusMessages.ps1
@@ -2,16 +2,15 @@
 # #################################################################################################################### #
 # Filename: \Intune\PowerShell Scripts\Disable-VerboseStatusMessages.ps1                                               #
 # Repository: Public                                                                                                   #
-# Created Date: Tuesday, October 1st 2024, 9:59:06 PM                                                                  #
-# Last Modified: Sunday, October 6th 2024, 11:21:33 PM                                                                 #
+# Created Date: Saturday, December 21st 2024, 6:43:29 PM                                                               #
+# Last Modified: Sunday, January 12th 2025, 10:35:16 PM                                                                #
 # Original Author: Darnel Kumar                                                                                        #
 # Author Github: https://github.com/Darnel-K                                                                           #
-# Github Org: https://github.com/ABYSS-ORG-UK/                                                                         #
 #                                                                                                                      #
 # This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90                           #
 #                                                                                                                      #
 # License: GNU General Public License v3.0 only - https://www.gnu.org/licenses/gpl-3.0-standalone.html                 #
-# Copyright (c) 2024 Darnel Kumar                                                                                      #
+# Copyright (c) 2024 - 2025 Darnel Kumar                                                                               #
 #                                                                                                                      #
 # This program is free software: you can redistribute it and/or modify                                                 #
 # it under the terms of the GNU General Public License as published by                                                 #
@@ -34,6 +33,33 @@
     & .\Disable-VerboseStatusMessages.ps1
 #>
 
+# Script functions
+
+function init {
+    $SCRIPT_EXEC_MODE = "Update" # Update or Delete. Tells the script to either update the registry or delete the keys
+    $REG_KEY_PATH = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+    $REG_DATA = @(
+        [PSCustomObject]@{
+            Path  = $REG_KEY_PATH
+            Key   = "verbosestatus"
+            Value = "0"
+            Type  = "DWord"
+        }
+        [PSCustomObject]@{
+            Path  = $REG_KEY_PATH
+            Key   = "DisableStatusMessages"
+            Value = "1"
+            Type  = "DWord"
+        }
+    )
+    if (-not (beginRegistryUpdate -data $REG_DATA -mode $SCRIPT_EXEC_MODE)) {
+        $CUSTOM_LOG.Fail("Unable to update registry data")
+        $CUSTOM_LOG.Error($Error)
+        Exit 1
+    }
+    Exit 0
+}
+
 #################################
 #                               #
 #   REQUIRED SCRIPT VARIABLES   #
@@ -44,22 +70,6 @@
 # DO NOT LEAVE THESE VARIABLES BLANK
 
 $SCRIPT_NAME = "Disable-VerboseStatusMessages"
-$SCRIPT_EXEC_MODE = "Update" # Update or Delete. Tells the script to either update the registry or delete the keys
-$REG_KEY_PATH = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-$REG_DATA = @(
-    [PSCustomObject]@{
-        Path  = $REG_KEY_PATH
-        Key  = "verbosestatus"
-        Value = "0"
-        Type  = "DWord"
-    }
-    [PSCustomObject]@{
-        Path  = $REG_KEY_PATH
-        Key  = "DisableStatusMessages"
-        Value = "1"
-        Type  = "DWord"
-    }
-)
 
 ################################################
 #                                              #
@@ -70,7 +80,11 @@ $REG_DATA = @(
 # Script functions - DO NOT CHANGE!
 
 function updateRegistry {
-    foreach ($i in ($REG_DATA | Sort-Object -Property Path)) {
+    param (
+        [Parameter()]
+        $data
+    )
+    foreach ($i in ($data | Sort-Object -Property Path)) {
         if (!(Test-Path -Path $i.Path)) {
             try {
                 New-Item -Path $i.Path -Force -ErrorAction Stop | Out-Null
@@ -79,7 +93,7 @@ function updateRegistry {
             catch {
                 $CUSTOM_LOG.Fail("Failed to create registry path: $($i.Path)")
                 $CUSTOM_LOG.Error($Error[0])
-                Exit 1
+                return $false
             }
         }
         if ($i.Key) {
@@ -91,7 +105,7 @@ function updateRegistry {
                 catch {
                     $CUSTOM_LOG.Fail("Failed to make the following registry edit:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
                     $CUSTOM_LOG.Error($Error[0])
-                    Exit 1
+                    return $false
                 }
             }
             else {
@@ -102,17 +116,21 @@ function updateRegistry {
                 catch {
                     $CUSTOM_LOG.Fail("Failed to make the following registry edit:`n - Key: $($i.Path)`n - Property: $($i.Key)`n - Value: $($i.Value)`n - Type: $($i.Type)")
                     $CUSTOM_LOG.Error($Error[0])
-                    Exit 1
+                    return $false
                 }
             }
         }
     }
     $CUSTOM_LOG.Success("Completed registry update successfully.")
-    Exit 0
+    return $true
 }
 
 function removeRegistry {
-    foreach ($i in ($REG_DATA | Sort-Object -Property Path, Key -Descending)) {
+    param (
+        [Parameter()]
+        $data
+    )
+    foreach ($i in ($data | Sort-Object -Property Path, Key -Descending)) {
         if (Test-Path -Path $i.Path) {
             if ($i.Key) {
                 try {
@@ -122,7 +140,7 @@ function removeRegistry {
                 catch {
                     $CUSTOM_LOG.Fail("Failed to remove registy property: $($i.Key) at path: $($i.Path)")
                     $CUSTOM_LOG.Error($Error[0])
-                    Exit 1
+                    return $false
                 }
             }
             else {
@@ -133,7 +151,7 @@ function removeRegistry {
                 catch {
                     $CUSTOM_LOG.Fail("Failed to remove registy path: $($i.Path)")
                     $CUSTOM_LOG.Error($Error[0])
-                    Exit 1
+                    return $false
                 }
             }
         }
@@ -142,14 +160,22 @@ function removeRegistry {
         }
     }
     $CUSTOM_LOG.Success("Completed registry update successfully.")
-    Exit 0
+    return $true
 }
 
-function init {
-    switch ($SCRIPT_EXEC_MODE) {
-        "Update" { updateRegistry }
-        "Delete" { removeRegistry }
-        Default { updateRegistry }
+function beginRegistryUpdate {
+    param (
+        [Parameter()]
+        $data,
+        [Parameter()]
+        [ValidateSet("Update", "Delete")]
+        [string]
+        $mode
+    )
+    switch ($mode) {
+        "Update" { return updateRegistry -data $data }
+        "Delete" { return removeRegistry -data $data }
+        Default { return updateRegistry -data $data }
     }
 }
 
@@ -383,4 +409,5 @@ class CustomLog {
 Clear-Host
 sig
 checkRunIn64BitPowershell
+$CUSTOM_LOG.Information("Script PID: $PID")
 init
